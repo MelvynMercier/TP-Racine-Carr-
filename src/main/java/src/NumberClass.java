24708ee097ca03fbec8118a30fa99370faf3b625
MelvynMercier <43468212+MelvynMercier@users.noreply.github.com>
2020-12-09 17:13:35 +0100

    Commit

--- src/main/java/src/NumberClass.java
+++ src/main/java/src/NumberClass.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.ArrayList;
+
 /** Class NumberNumberClass */
 public class NumberClass {
     
@@ -9,53 +11,35 @@ public class NumberClass {
      * @return list (Array<float>)
 	 * @throws Exception 
      */
-    public static float[] ListOfSquareRoot(int numberA, int numberB) throws Exception{
-        CheckInteger(numberA, numberB);
-        CheckSuperior(numberA, numberB);
+    public static ArrayList<Float> ListOfSquareRoot(int numberA, int numberB) throws Exception{
+//        CheckSuperior(numberA, numberB);
+        ArrayList<Float> list = new ArrayList<Float>();
         
-        float[] list = new float[CalculGap(numberA, numberB)];
-        
-        //TODO
-        //Le reste
-        
+        for (int i = numberA; i <= numberB; i++) {
+        	 list.add(FindSquareRoot(i));
+        }
         
         return list;
     }
+
+    
+//	/** Check if the A value is less than the B value
+//     * @param numberA (int)
+//     * @param numberB (int)
+//	 * @throws Exception 
+//     */
+//    private static void CheckSuperior(int numberA, int numberB) throws Exception{
+//        if(numberA > numberB) {
+//        	throw new Exception("La valeur A est sup�rieur � la valeur B");
+//        }
+//    }
     
     
-    
-	/** Check if the 2 values are integers
-     * @param numberA (int)
-     * @param numberB (int)
-	 * @throws Exception 
+	/** Calcul square root od number
+     * @param number (int)
+     * @return square root (float)
      */
-    private static void CheckInteger(int numberA, int numberB) throws Exception{
-        try {
-            Integer.parseInt(String.valueOf(numberA));
-            Integer.parseInt(String.valueOf(numberB));
-        }
-        catch(Exception e) {
-            throw new Exception("Une des 2 entr�e n'est pas un entier");
-        }
-    }
-    
-	/** Check if the A value is less than the B value
-     * @param numberA (int)
-     * @param numberB (int)
-	 * @throws Exception 
-     */
-    private static void CheckSuperior(int numberA, int numberB) throws Exception{
-        if(numberA > numberB) {
-        	throw new Exception("La valeur A est sup�rieur � la valeur B");
-        }
-    }
-    
-	/** Calculation of the difference between 2 integers
-     * @param numberA (int)
-     * @param numberB (int)
-     * @return gap (int)
-     */
-    private static int CalculGap(int numberA, int numberB){
-        return numberB - numberA;
+    private static float FindSquareRoot(int number) {
+    	return (float) Math.sqrt(number);
     }
 }
--- src/main/java/test/NumberTest.java
+++ src/main/java/test/NumberTest.java
@@ -1,15 +1,62 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import src.NumberClass;
 
-
 /** Class test of NUMBER class */
 public class NumberTest {
 	
+	int numberA = 30;
+	int numberB = 66;
+	int checkNumber = 30;
+	ArrayList<Float> list = new ArrayList<Float>();
+	
+	
+	/** Before test we check if each value is an integer
+	 * @throws Exception 
+     */
+	@Test
+	@Before
+	public void CheckInteger() throws Exception {
+        try {
+            Integer.parseInt(String.valueOf(this.numberA));
+            Integer.parseInt(String.valueOf(this.numberB));
+        }
+        catch(Exception e) {
+            throw new Exception("Une des 2 valeur entrée n'est pas un entier");
+        }
+	}
+	
+	/** Before test we check if the A value is less than the B value
+	 * @throws Exception 
+     */
+	@Test
+	@Before
+	public void CheckSuperior() throws Exception {
+        if(numberA > numberB) {
+        	throw new Exception("La valeur A est supérieur à la valeur B");
+        }
+	}
+	
 	@Test
 	public void FirstTest() throws Exception {
-		NumberClass.ListOfSquareRoot(40, 50);
+		this.list = NumberClass.ListOfSquareRoot(this.numberA, this.numberB);
+		assertEquals((float) Math.sqrt(this.checkNumber), list.get(this.checkNumber - this.numberA));
+	}
+	
+	@Test
+	@After
+	public void CheckEqual() {
+		System.out.println(this.list.size());
+		System.out.println(this.numberA - this.numberB);
+		assertEquals(this.list.size(), this.numberB - this.numberA);
 	}
 }
